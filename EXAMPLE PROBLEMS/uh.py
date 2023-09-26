2ffa4f74ae8631abf725bcdd1b1a62d4c49647b6
Chrisawgey <124189207+Chrisawgey@users.noreply.github.com>
2023-09-26 10:43:56 -0400

    takin notes

---
+++ EXAMPLE PROBLEMS/uh.py
@@ -0,0 +1,3 @@
+import math
+area =23.55
+print("the radious is %0.3f" % area)
---
+++ EXAMPLE PROBLEMS/uhfixed.py
@@ -0,0 +1,7 @@
+import math
+
+area = 23.55
+radius = math.sqrt(area / math.pi)
+formatted_radius = "%.3f" % radius
+
+print("The radius is", formatted_radius)
